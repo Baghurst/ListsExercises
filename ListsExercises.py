65f9f00c7834adabb3bed999df75f7bcbdb75403
Baghurst <23bbaghurst.acc@lwetb.ie>
2025-02-11 13:39:28 +0000

    Update ListsExercises.py

--- ListsExercises.py
+++ ListsExercises.py
@@ -14,11 +14,38 @@ print(L1)
 LL = list("aaa")
 print(LL)
 
+L1= list(input("Enter list elements: ")) 
+print(L1)
+list = eval(input("Enter list to be added: " ))
+print ("list you entered :", list)
 
+J = eval('5+8')
+print(J)
 
+y= eval("3*10")
+print(y)
 
+y= eval("2+2")
+print(y)
 
+var1 = eval(input("Enter Value:"))
+print(var1,type(var1))
 
+L = ['P','y','t','h','o','n']
+for a in L :
+    print(a)
 
+L=['q','w','e','r','t','y']
+lenght = len(L)
+for a in range(lenght) :
+    print("At indexes", a, "and", (a - lenght), "element :", L[a])
+
+X=L+L1
+print(X)
+
+lst =[10,12,14,20,22,24,30,32,34]
+seq = lst [3:-3]
+seq
+print(lst)
 
 
